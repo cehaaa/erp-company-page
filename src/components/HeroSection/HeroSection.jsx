cb8fdd2689557e7a7586f22b14f9f9902d87e73f
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-27 17:39:41 +0700

    feat: update hero image

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,7 +2,12 @@ import Button from "../Basics/Button/Button";
 
 const HeroSection = () => {
 	return (
-		<div className='w-full min-h-[calc(100vh-128px)] bg-hero-section bg-cover relative'>
+		<div
+			className='w-full min-h-[calc(100vh-128px)] bg-cover relative'
+			style={{
+				backgroundImage:
+					'url("https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80")',
+			}}>
 			<div className='absolute w-full h-full top-0 left-0 bg-blue-700 bg-opacity-50 flex items-center justify-center'>
 				<div className='md:w-6/12 text-center text-white'>
 					<div className='uppercase font-semibold tracking-wider'>
